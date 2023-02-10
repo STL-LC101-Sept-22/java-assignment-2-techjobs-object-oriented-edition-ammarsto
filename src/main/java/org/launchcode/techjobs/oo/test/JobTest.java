1f55caf15079b7bbd887880c272ba5740fdc6f24
Amy Stockhausen <stockhausenamy@gmail.com>
2023-02-09 19:15:32 -0600

    Completed task 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,45 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob1,testJob2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob1 instanceof Job);
+        assertTrue(testJob1.getEmployer() instanceof Employer);
+        assertTrue(testJob1.getLocation() instanceof Location);
+        assertTrue(testJob1.getPositionType() instanceof PositionType);
+        assertTrue(testJob1.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(testJob1.getName(),"Product tester");
+        assertEquals(testJob1.getEmployer().toString(), "ACME");
+        assertEquals(testJob1.getLocation().toString(), "Desert");
+        assertEquals(testJob1.getPositionType().toString(), "Quality Control");
+        assertEquals(testJob1.getCoreCompetency().toString(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertNotEquals(testJob1,testJob2);
+    }
 
 }
